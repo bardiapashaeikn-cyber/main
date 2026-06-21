341703de04be7516725caa46cc69434560fe96d4
bardiapashaeikn-cyber <bardiapashaeik.n@gmail.com>
2026-06-21 19:26:02 +0330

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Quant Dashboard",
+    layout="wide"
+)
+
+st.title("📊 Quant Trading Dashboard")
+
+st.write("""
+Welcome 👋
+
+Use the sidebar to navigate between tools:
+
+- Chart Lookalike Engine
+- Monte Carlo future price Simulation
+- Portfolio return Analysis
+- Exceed prev Strategy App
+""")
