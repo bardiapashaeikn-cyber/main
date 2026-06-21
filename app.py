6a98e680997095c105c1539594cee53e85cccda8
bardiapashaeikn-cyber <bardiapashaeik.n@gmail.com>
2026-06-21 19:48:34 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Quant Dashboard",
-    layout="wide"
-)
+st.set_page_config(page_title="Quant Dashboard", layout="wide")
 
-st.title("📊 Quant Trading Dashboard")
+st.title("📊 Quant Dashboard")
 
-st.write("""
-Welcome 👋
-
-Use the sidebar to navigate between tools:
-
-- Chart Lookalike Engine
-- Monte Carlo future price Simulation
-- Portfolio return Analysis
-- Exceed prev Strategy App
-""")
+st.write("Use sidebar to switch between apps")
