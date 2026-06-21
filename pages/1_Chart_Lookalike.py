bf3bf62c3a92af0aecd57113930fa7ed9686870f
bardiapashaeikn-cyber <bardiapashaeik.n@gmail.com>
2026-06-21 19:49:28 +0330

    Update 1_Chart_Lookalike.py

--- pages/1_Chart_Lookalike.py
+++ pages/1_Chart_Lookalike.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.set_page_config(layout="wide")
+
 st.title("📊 Market Pattern Look-Alike Engine")
 
 # =========================
