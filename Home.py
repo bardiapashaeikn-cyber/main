7141be4dc0f274784b246bdb5ba1f83a3ecf1cf2
bardiapashaeikn-cyber <bardiapashaeik.n@gmail.com>
2026-07-05 02:47:48 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,3 +5,4 @@ st.set_page_config(page_title="Quant Dashboard", layout="wide")
 st.title("📊 Quant Dashboard")
 
 st.write("Use sidebar to switch between apps")
+st.write("THIS THIS THIS IS A TEST , WHATUP BIATCH")
