2ed0e93596690aafbe6fbd71eaf5838d3c8b09b7
bardiapashaeikn-cyber <bardiapashaeik.n@gmail.com>
2026-07-05 02:50:34 +0330

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,6 +5,5 @@ st.set_page_config(page_title="Quant Dashboard", layout="wide")
 st.title("📊 Quant Dashboard")
 
 st.write("You are welcomed to use that fucking side bar on your left , or don't , IDGAF , Actually i do , so 
-fuck ,
-         you ")
+fuck ,you ")
 st.write("THIS THIS THIS IS A TEST , WHATUP BIATCH")
